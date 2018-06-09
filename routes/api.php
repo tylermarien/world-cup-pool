d7f275eeb590243cf60cd1790704944e74d4b0bf
Craig Paul <craig.paul@coconutcalendar.com>
2018-06-09 15:09:52 -0600

    Changes to correct resource route method

--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,8 @@ use Illuminate\Http\Request;
 */
 
 Route::namespace('Api')->middleware('auth:api')->group(function () {
-    Route::resource('entries', 'EntryController')->except(['create', 'edit']);
-    Route::resource('players', 'PlayerController')->except(['create', 'edit']);
-    Route::resource('pools', 'PoolController')->except(['create', 'edit']);
-    Route::resource('teams', 'TeamController')->except(['create', 'edit']);
+    Route::apiResource('entries', 'EntryController');
+    Route::apiResource('players', 'PlayerController');
+    Route::apiResource('pools', 'PoolController');
+    Route::apiResource('teams', 'TeamController');
 });
