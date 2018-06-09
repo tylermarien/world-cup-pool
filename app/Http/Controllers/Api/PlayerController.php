a7cb8070890ed889872495427cc5c2ae8d5091de
Craig Paul <craig.paul@coconutcalendar.com>
2018-06-09 14:38:13 -0600

    Adds player endpoints

--- app/Http/Controllers/Api/PlayerController.php
+++ app/Http/Controllers/Api/PlayerController.php
@@ -2,63 +2,93 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Player;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use App\Http\Controllers\Controller;
 
 class PlayerController extends Controller
 {
     /**
+     * @var \App\Player
+     */
+    protected $player;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @param \App\Player $player
+     *
+     * @return void
+     */
+    public function __construct(Player $player)
+    {
+        $this->player = $player;
+    }
+
+    /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
      */
     public function index()
     {
-        //
+        return $this->player->paginate();
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \App\Player|\Illuminate\Database\Eloquent\Model
      */
     public function store(Request $request)
     {
-        //
+        return $this->player->create($request->all());
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param int $id
+     *
+     * @return \Illuminate\Database\Eloquent\Model
      */
     public function show($id)
     {
-        //
+        return $this->player->findOrFail($id);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param \Illuminate\Http\Request $request
+     * @param int                      $id
+     *
+     * @return \App\Player|\Illuminate\Database\Eloquent\Model
      */
     public function update(Request $request, $id)
     {
-        //
+        $player = $this->player->findOrFail($id);
+
+        $player->update($request->all());
+
+        return $player;
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param int $id
+     *
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Exception
      */
     public function destroy($id)
     {
-        //
+        $this->player->findOrFail($id)->delete();
+
+        return response()->json('', Response::HTTP_NO_CONTENT);
     }
 }
