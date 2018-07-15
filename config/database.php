99c69f1729fc89528598759304f90416e50078d3
Tyler Marien <tyler@coconutcalendar.com>
2018-07-15 09:14:31 -0600

    Share db config other than name

--- config/database.php
+++ config/database.php
@@ -41,12 +41,17 @@ return [
 
         'sportdb' => [
             'driver' => 'mysql',
-            'host' => env('SPORTDB_HOST', '127.0.0.1'),
-            'port' => env('SPORTDB_PORT', '3306'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
             'database' => env('SPORTDB_DATABASE', 'forge'),
-            'username' => env('SPORTDB_USERNAME', 'forge'),
-            'password' => env('SPORTDB_PASSWORD', ''),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
+            'strict' => true,
+            'engine' => null,
         ],
 
         'mysql' => [
