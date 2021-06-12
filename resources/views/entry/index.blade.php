875d77e55a0c0ef6f22a5f0a473a3046c47345b1
Tyler Marien <tyler.marien@gmail.com>
2021-06-11 21:47:16 -0600

    upgrade to laravel 6

--- resources/views/entry/index.blade.php
+++ resources/views/entry/index.blade.php
@@ -10,21 +10,21 @@
                         <div class="featured gold">
                             <img src="{{ asset('img/gold-ribbon.svg') }}" class="ribbon">
                             <div class="container">
-                                <p><a href="{{ route('entries.show', ['entry' => $first]) }}">{{ $first->name  }}</a> <small>{{ __('(:played gp)', ['played' => $first->calculateGamesPlayed()]) }} - {{ __(':total pts', ['total' => $first->total]) }}</small></p>
+                                <p><a href="{{ route('entries.show', ['id' => $first]) }}">{{ $first->name  }}</a> <small>{{ __('(:played gp)', ['played' => $first->calculateGamesPlayed()]) }} - {{ __(':total pts', ['total' => $first->total]) }}</small></p>
                             </div>
                         </div>
 
                         <div class="featured silver">
                             <img src="{{ asset('img/silver-ribbon.svg') }}" class="ribbon">
                             <div class="container">
-                            <p><a href="{{ route('entries.show', ['entry' => $second]) }}">{{ $second->name  }}</a> <small>{{ __('(:played gp)', ['played' => $second->calculateGamesPlayed()]) }} - {{ __(':total pts', ['total' => $second->total]) }}</small></p>
+                            <p><a href="{{ route('entries.show', ['id' => $second]) }}">{{ $second->name  }}</a> <small>{{ __('(:played gp)', ['played' => $second->calculateGamesPlayed()]) }} - {{ __(':total pts', ['total' => $second->total]) }}</small></p>
                             </div>
                         </div>
 
                         <div class="featured bronze">
                             <img src="{{ asset('img/bronze-ribbon.svg') }}" class="ribbon">
                             <div class="container">
-                            <p><a href="{{ route('entries.show', ['entry' => $third]) }}">{{ $third->name }}</a> <small>{{ __('(:played gp)', ['played' => $third->calculateGamesPlayed()]) }} - {{ __(':total pts', ['total' => $third->total]) }}</small></p>
+                            <p><a href="{{ route('entries.show', ['id' => $third]) }}">{{ $third->name }}</a> <small>{{ __('(:played gp)', ['played' => $third->calculateGamesPlayed()]) }} - {{ __(':total pts', ['total' => $third->total]) }}</small></p>
                             </div>
                         </div>
 
@@ -38,7 +38,7 @@
                             <tbody>
                                 @foreach($entries as $entry)
                                 <tr>
-                                    <td><a href="{{ route('entries.show', ['entry' => $entry]) }}">{{ $entry->name }}</a> {{ __('(:played gp)', ['played' => $entry->calculateGamesPlayed()]) }}</td>
+                                    <td><a href="{{ route('entries.show', ['id' => $entry]) }}">{{ $entry->name }}</a> {{ __('(:played gp)', ['played' => $entry->calculateGamesPlayed()]) }}</td>
                                     <td class="text-right">{{ __(':total pts', ['total' => $entry->total]) }}</td>
                                 </tr>
                                 @endforeach
